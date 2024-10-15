400d766c438d8c29da41cf8d248a8f3e40a4a8fd
KrispinMartin <kmar0042@student.monash.edu>
2024-10-16 00:18:03 +1100

    created html file

---
+++ vega_lite_vis.js
@@ -0,0 +1,34 @@
+var vg_1 = "scatterplot/scatterplot.vg.json";
+
+vegaEmbed("#scatter_plot", vg_1).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+    }).catch(console.error);
+
+var vg_2 = "resilience_factors/radarplot1.vg.json";
+
+vegaEmbed("#radar_plot1", vg_2).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+    }
+    ).catch(console.error);
+
+var vg_3 = "criminality_factors/radarplot2.vg.json";
+
+vegaEmbed("#radar_plot2", vg_3).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+    }
+    ).catch(console.error);
+
+var vg_4 = "ausmap/prisonersausmap.vg.json";
+
+vegaEmbed("#prisoners_aus_map", vg_4).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+    }
+    ).catch(console.error);
+
+var vg_5 = "viccrime/viccrime.vg.json";
+
+vegaEmbed("#vic_crime", vg_5).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+    }
+    ).catch(console.error);
+
