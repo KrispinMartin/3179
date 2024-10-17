274e53db7257b804b81f3b9e259b1a66076206a6
KrispinMartin <kmar0042@student.monash.edu>
2024-10-18 00:17:45 +1100

    newmap

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -18,9 +18,9 @@ vegaEmbed("#radar_plot2", vg_3, {actions: false}).then(function(result) {
     }
     ).catch(console.error);
 
-var vg_4 = "ausmap/prisonersausmap.vg.json";
+var vg_4 = "ausmap/cybercrimesmap.vg.json";
 
-vegaEmbed("#prisoners_aus_map", vg_4, {actions: false}).then(function(result) {
+vegaEmbed("#cyber_crime", vg_4, {actions: false}).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
     }
     ).catch(console.error);
