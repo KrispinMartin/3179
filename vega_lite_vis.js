c6a1f0ec2560d620a3d9354cc6f4292583096229
KrispinMartin <kmar0042@student.monash.edu>
2024-10-17 21:16:35 +1100

    building html and css

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,40 +1,54 @@
 var vg_1 = "scatterplot/scatterplot.vg.json";
 
-vegaEmbed("#scatter_plot", vg_1).then(function(result) {
+vegaEmbed("#scatter_plot", vg_1, {actions: false}).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
     }).catch(console.error);
 
 var vg_2 = "resilience_factors/radarplot1.vg.json";
 
-vegaEmbed("#radar_plot1", vg_2).then(function(result) {
+vegaEmbed("#radar_plot1", vg_2, {actions: false}).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
     }
     ).catch(console.error);
 
 var vg_3 = "criminality_factors/radarplot2.vg.json";
 
-vegaEmbed("#radar_plot2", vg_3).then(function(result) {
+vegaEmbed("#radar_plot2", vg_3, {actions: false}).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
     }
     ).catch(console.error);
 
 var vg_4 = "ausmap/prisonersausmap.vg.json";
 
-vegaEmbed("#prisoners_aus_map", vg_4).then(function(result) {
+vegaEmbed("#prisoners_aus_map", vg_4, {actions: false}).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
     }
     ).catch(console.error);
 
 var vg_5 = "viccrime/treemap.vg.json";
 
-vegaEmbed("#vic_crime", vg_5).then(function(result) {
+vegaEmbed("#vic_crime", vg_5, {actions: false}).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
     }
     ).catch(console.error);
 
 var vg_6 = "worldmap/criminalityworldmap.vg.json";
 
-vegaEmbed("#world_map", vg_6).then(function(result) {
+vegaEmbed("#world_map", vg_6, {actions: false}).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+    }
+    ).catch(console.error);
+
+var vg_7 = "sankey/drugsankeychart.vg.json";
+
+vegaEmbed("#sankey_drugs", vg_7, {actions: false}).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+    }
+    ).catch(console.error);
+
+var vg_8 = "barchart/drugtypebar.vg.json";
+
+vegaEmbed("#bar_drugs", vg_8, {actions: false}).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
     }
     ).catch(console.error);
